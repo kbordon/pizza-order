87e300de629498cd1191d32e7a1e21042f338427
Kimberly Bordon <kbordon@gmail.com>
2017-09-29 10:34:22 -0700

    Add functionality to inquire preferred pizza size.

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,13 @@ function Pizza(item, size){
 $(document).ready(function() {
   $("#pizza-form").submit(function(event){
     event.preventDefault();
-    var pizzaTopping;
+    var pizzaTopping = [];
     $("input:checkbox[name=regular-topping]:checked").each(function() {
-      pizzaTopping = $(this).val();
+      pizzaTopping.push($(this).val());
     });
 
-    alert(pizzaTopping);
-
     var pizzaSize = $("input:radio:checked").val();
+
     var pizzaOrder = new Pizza(pizzaTopping, pizzaSize);
     console.log(pizzaOrder);
   });
